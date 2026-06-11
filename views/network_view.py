eae1de00b2159320fc6a494f8ffebb6af6ec7a32
Noslia <ailson.gabriel.sv@gmail.com>
2026-06-10 23:42:20 -0300

    feat(network): implement network information module

--- views/network_view.py
+++ views/network_view.py
@@ -3,12 +3,27 @@ from views.base_view import BaseView
 class NetworkView(BaseView):
   def show_network_info(self, network_info):
     print("\n======================== Network =========================")
-    print(f"IP Local            = {network_info.ip_local}")
-    print(f"Hostname            = {network_info.hostname}")
-    print(f"Gateway             = {network_info.gateway}")
-    for i, interface in enumerate(network_info.interfaces):
-        print(f"Interface {i}         = {interface}")
-    print(f"Downloads           = {self.format_size(network_info.download_bytes)}")
-    print(f"Uploads             = {self.format_size(network_info.upload_bytes)}")
-    print(f"IP Público          = {network_info.public_ip}")
-  
+    print(f"IP Local    = {network_info.ip_local}")
+    print(f"Hostname    = {network_info.hostname}")
+
+    if network_info.gateway is not None:
+      print(f"Gateway     = {network_info.gateway}")
+
+    print(f"Downloads   = {self.format_size(network_info.download_bytes)}")
+    print(f"Uploads     = {self.format_size(network_info.upload_bytes)}")
+
+    if network_info.public_ip is not None: 
+      print(f"IP Público  = {network_info.public_ip}")
+
+    if network_info.primary_interface is not None:
+      print(f"Interface de Rede Principal = {network_info.primary_interface}")
+
+    if network_info.vpn_status is not None:
+      print(f"VPN Status                  = {network_info.vpn_status}")
+
+    print("\n------------------ Interfaces de Rede: ------------------")
+    for iface, ips in network_info.interfaces.items():
+      print(f"\nInterface: {iface}")
+      for ip in ips:
+        print(f"  IPv4 -> {ip}")
+    print("-----------------------------------------------------------")
